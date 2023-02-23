982a536d9c4f744983460260290de5df6fa6dc14
Muhammad Lukman <lukmanmsoediro@gmail.com>
2023-02-23 20:14:53 +0700

    ubah

---
+++ JS/index.js
@@ -0,0 +1,21 @@
+const nama = prompt("Silahkan masukan nama anda: ")
+document.getElementById("nama").innerText = nama;
+
+function validasiForm() {
+    var nama = document.forms["form"]["nama"].value;
+    var tglLahir = document.forms["form"]["tanggal-lahir"].value;
+    var jk = document.forms["form"]["gender"].value;
+    var pesan = document.forms["form"]["pesan"].value;
+
+    if (nama == "" || tglLahir == "" || jk == "" || pesan == "") {
+        alert('input tidak boleh kosong');
+        return false;
+    }
+
+    document.getElementById("out-nama").innerText = nama;
+    document.getElementById("out-tgllahir").innerText = tglLahir;
+    document.getElementById("out-gender").innerHTML = jk;
+    document.getElementById("out-pesan").innerText = pesan;
+
+    return false;
+}
